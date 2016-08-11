84d698a3333e07da625ebde6ad4ed6e302f37c30
LanaLaw <elawrence.blain@gmail.com>
2016-08-11 13:43:09 -0400

    First commit

---
+++ Woke/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Woke
+//
+//  Created by EJL on 8/10/16.
+//  Copyright © 2016 MEC Revolutionaries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
