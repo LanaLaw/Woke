e641d2ee6829096b03e0a37f35c7e20597dc5e87
LanaLaw <elawrence.blain@gmail.com>
2016-08-15 14:31:00 -0400

    upload pod

---
+++ Woke/BaseService.swift
@@ -0,0 +1,9 @@
+//
+//  BaseService.swift
+//  Woke
+//
+//  Created by EJL on 8/15/16.
+//  Copyright © 2016 MEC Revolutionaries. All rights reserved.
+//
+
+import Foundation
---
+++ Woke/SignUpPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpPageViewController.swift
+//  Woke
+//
+//  Created by EJL on 8/12/16.
+//  Copyright © 2016 MEC Revolutionaries. All rights reserved.
+//
+
+import UIKit
+
+class SignUpPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
